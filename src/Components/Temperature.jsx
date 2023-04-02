9a215a247fd892d1e6e4220616485b9e02df62c3
wallace-y <cameronwfinney@gmail.com>
2023-04-02 12:44:13 +0100

    add working colour change function

--- src/Components/Temperature.jsx
+++ src/Components/Temperature.jsx
@@ -2,14 +2,42 @@ import React, {useState} from 'react';
 
 function TemperatureCounter() {
     // Define the count variable
-    const [count,setCount] = useState(20);
+    let [count,setCount] = useState(20);
+    let [color,setColor] = useState("red")
+
+    function decreaseTemp() {
+        if (count > 0) {
+             setCount(count - 1);
+        } else {
+            // Don't update the count
+        }
+        setBG()
+    }
+
+    function increaseTemp() {
+        if (count > 0) {
+             setCount(count + 1);
+        } else {
+            // Don't update the count
+        }
+        setBG()
+
+    }
+
+    function setBG() {
+        if(count < 15) {
+            setColor("blue")
+        } else {
+            setColor("red")
+        }
+    }
 
 
     return (
-        <div>
+        <div class={color}>
             <h1>Current temperature: {count}</h1>
-            <button onClick={() => setCount(count > 1 ? count - 1 : 0)}>Decrease Temp</button>
-            <button onClick={() => setCount(count + 1)}>Increase Temp</button>
+            <button onClick={decreaseTemp}>Decrease Temp</button>
+            <button onClick={increaseTemp}>Increase Temp</button>
         </div>
     )
 }
